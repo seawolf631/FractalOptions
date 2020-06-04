5d64e76a3238d90587b5bb50b07f82cecbfb8a4b
user <andrewwyman27@gmail.com>
2020-06-03 23:52:46 -0400

    mvp of survival plot done

---
+++ src/generatePlot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import psycopg2
+
+def getData():
+    conn = psycopg2.connect(host="localhost", port = 5432, database = , user="postgres", password=)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("select (open-low)/open as percent_difference from historical_data order by percent_difference ASC;")
+        queryResult = cur.fetchall()
+        cur.execute("select count(*) from historical_data")
+        rowLength = cur.fetchall()
+    cur.close()
+    conn.close()
+    return queryResult, rowLength
+
+x,y = getData()
+y=y*y[0][0]
+newY = []
+n=0
+i=0
+for k in y:
+    for j in k:
+        newValue = (j-i)/(j)
+        newY += (newValue,)
+        i += 1
+    n += 1
+plt.xscale('log')
+plt.yscale('log')
+plt.plot(x,newY)
+plt.savefig('static/plot.png')
--- src/main.py
+++ src/main.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+import psycopg2
+import matplotlib.pyplot as plt
+import io
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world'
+
+def getData():
+    conn = psycopg2.connect(host="localhost", port = 5432, database = , user="postgres", password=)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("select (open-low)/open as percent_difference from historical_data order by percent_difference DESC limit 10;")
+        queryResult = cur.fetchall()
+        cur.execute("select count(*) from historical_data")
+        rowLength = cur.fetchall()
+    cur.close()    
+    conn.close()
+    return queryResult, rowLength
+        
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/", methods=['POST'])
+def get_stock_ticker():
+    stockTicker = request.form['stockTicker']
+    return stockTicker
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port = 80, debug=True)
